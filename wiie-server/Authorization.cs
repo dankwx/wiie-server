5762ccf26fdc6d568d1442638f8dfd5305d1468f
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-03-20 17:46:32 -0300

    chore: add comments

---
+++ wiie-server/Authorization.cs
@@ -0,0 +1,37 @@
+﻿using Npgsql;
+using System;
+
+public class Autenticacao
+{
+    public static bool FazerLogin()
+    {
+        Console.WriteLine("Digite seu e-mail:");
+        string email = Console.ReadLine();
+
+        Console.WriteLine("Digite sua senha:");
+        string senha = Console.ReadLine();
+
+        return AutenticarUsuario(email, senha);
+    }
+
+    private static bool AutenticarUsuario(string email, string senha)
+    {
+        string queryString = "SELECT COUNT(*) FROM Usuarios WHERE Email = @Email AND Senha = @Senha";
+        int result = 0;
+
+        using (NpgsqlConnection connection = new NpgsqlConnection(DatabaseConfig.ConnectionString))
+        {
+            connection.Open();
+
+            using (NpgsqlCommand command = new NpgsqlCommand(queryString, connection))
+            {
+                command.Parameters.AddWithValue("@Email", email);
+                command.Parameters.AddWithValue("@Senha", senha);
+
+                result = Convert.ToInt32(command.ExecuteScalar());
+            }
+        }
+
+        return result > 0;
+    }
+}
--- wiie-server/Program.cs
+++ wiie-server/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        MenuOptions.MenuList();
+        Console.WriteLine("Digite seu e-mail:");
+        string email = Console.ReadLine();
+
+        Console.WriteLine("Digite sua senha:");
+        string senha = Console.ReadLine();
+
+        if (!Autenticacao.FazerLogin());
+        {
+            Console.WriteLine("Usuário autenticado com sucesso!");
+            MenuOptions.MenuList(); // Executar o menu apenas se o usuário estiver autenticado
+        }
+        else
+        {
+            Console.WriteLine("Email ou senha incorretos. Tente novamente.");
+        }
     }
 }
